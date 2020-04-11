ad45018c48e146177dd7f4a05dbd3a5d057fdeff
Kott Mihaela <floricamihaela10@gmail.com>
2020-04-11 13:18:29 +0300

    removed bad imports
    added memory container to calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {Counter} from './counter/Counter';
-import {Calculator} from './calculator/Calculator';
 import {RealCalculator} from './real-calculator/RealCalculator';
 
 function App() {
